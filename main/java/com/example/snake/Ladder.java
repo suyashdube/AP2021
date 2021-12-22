ec581086a404e2a9ed8adc8a9139c6ff764cb979
suyashdube <88726963+suyashdube@users.noreply.github.com>
2021-12-22 19:44:17 +0300

    Delete main directory

--- main/java/com/example/snake/Ladder.java
+++
@@ -1,8 +0,0 @@
-package com.example.snake;
-
-public class Ladder extends Obstacle{
-
-    public Ladder(boolean isForward, double initposX, double initposY, double finalposX, double finalposY, int pos) {
-        super(isForward,initposX,initposY,finalposX,finalposY,pos);
-    }
-}
--- main/java/com/example/snake/Snake.java
+++
@@ -1,8 +0,0 @@
-package com.example.snake;
-
-public class Snake extends Obstacle {
-
-    public Snake(boolean isForward, double initposX, double initposY, double finalposX, double finalposY, int pos) {
-        super(isForward,initposX,initposY,finalposX,finalposY,pos);
-    }
-}
